56be88b8d97119cce32d4cf8da66f8fc716453ac
rakeshjto <rakesh26061984@gmail.com>
2018-09-05 13:10:58 +0530

    LL Only removed in FTTH as per the directions of GMTD

--- gross_net_sde.php
+++ gross_net_sde.php
@@ -165,10 +165,8 @@ group by A.SDE,C.BB_TAR ORDER BY SUBSTR(A.SDE,-3)";
 		$sql= "SELECT * FROM 
 (SELECT SUBSTR(SDE,-3) SDE,SUM(FTTH_TAR) FTTH_TAR FROM MON_TARGETS_SDE_WISE WHERE  TRUNC(MON_YY) BETWEEN '" .$first_day_of_fdate. "' AND '" .$first_day_of_tdate. "' GROUP BY SUBSTR(SDE,-3)) A,
 		(SELECT SUBSTR(A.SDE,-3) SDE, 
-COUNT(DISTINCT (CASE WHEN ORDER_TYPE='New' AND ORDER_SUB_TYPE='Provision' AND CUST_ACCNT_NO NOT IN (" . $data2 . "'123') AND PHONE_NO NOT IN (" . $avoid_duplicate_data . "'123') THEN PHONE_NO END)) LLONLYPROV,
 COUNT(DISTINCT (CASE WHEN ORDER_TYPE='New' AND ORDER_SUB_TYPE='Broadband Provision' AND CUST_ACCNT_NO NOT IN (" . $data2 . "'123') AND PHONE_NO NOT IN (" . $avoid_duplicate_data . "'123') THEN PHONE_NO END)) BBONLYPROV,
 COUNT(DISTINCT (CASE WHEN ORDER_TYPE='New' AND CUST_ACCNT_NO  IN (" . $data2 . "'123') AND PHONE_NO NOT IN (" . $avoid_duplicate_data . "'123') THEN PHONE_NO END)) PROV,
-COUNT(DISTINCT (CASE WHEN ORDER_TYPE='Disconnect' AND SERVICE_SUB_TYPE='FTTH Voice' AND CUST_ACCNT_NO NOT IN (" . $data4 . "'123') THEN PHONE_NO END)) LLONLYDIS,
 COUNT(DISTINCT (CASE WHEN ORDER_TYPE='Disconnect' AND SERVICE_SUB_TYPE='FTTH BroadBand' AND CUST_ACCNT_NO NOT IN (" . $data4 . "'123') THEN PHONE_NO END)) BBONLYDIS,
 COUNT(DISTINCT (CASE WHEN ORDER_TYPE='Disconnect' AND CUST_ACCNT_NO  IN (" . $data4 . "'123') THEN PHONE_NO END)) DIS
 FROM EXCHANGE_CODE A
@@ -189,12 +187,12 @@ WHERE A.SDE=B.SDE";
 		echo "<th rowspan='2'>Sl No</th>";
 		echo "<th rowspan='2'>Station</th>";
 		echo "<th rowspan='2'>Gross Target</th>";
-		echo "<th colspan='3'>Provisions</th>";
-		echo "<th colspan='3'>Disconnetions</th>";
+		echo "<th colspan='2'>Provisions</th>";
+		echo "<th colspan='2'>Disconnetions</th>";
 		echo "<th rowspan='2'>Net<br>Achievement</th>";
 		echo "</tr>";
-		echo "<tr><th>LL Only</th><th>BB Only</th><th>LL+BB</th>";
-		echo "<th>LL Only</th><th>BB Only</th><th>LL+BB</th></tr>";
+		echo "<tr><th>BB Only</th><th>LL+BB</th>";
+		echo "<th>BB Only</th><th>LL+BB</th></tr>";
 		echo '</thead>';
 		
 		$ftth_tar= 0;
@@ -213,20 +211,16 @@ WHERE A.SDE=B.SDE";
 			echo "<td>" .$sno. "</td>";
 			echo "<td><a href='gross_net_exch.php?sde=" . $data['SDE'] . "&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH' target='_blank'>" .$data['SDE']. "</a></td>";
 			echo "<td>" .$data['FTTH_TAR']. "</td>";
-			echo "<td><a href='gross_net_det.php?sde=SSA&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH&order_type=PROV&order_Sub_type=LL&sdca=" .$data['SDE']. "' target='_blank'>" .$data['LLONLYPROV']. "</a></td>";
 			echo "<td><a href='gross_net_det.php?sde=SSA&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH&order_type=PROV&order_Sub_type=BB&sdca=" .$data['SDE']. "' target='_blank'>" .$data['BBONLYPROV']. "</a></td>";
 			echo "<td><a href='gross_net_det.php?sde=SSA&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH&order_type=PROV&order_Sub_type=LLBB&sdca=" .$data['SDE']. "' target='_blank'>" .$data['PROV']. "</a></td>";
-			echo "<td><a href='gross_net_det.php?sde=SSA&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH&order_type=DIS&order_Sub_type=LL&sdca=" .$data['SDE']. "' target='_blank'>" .$data['LLONLYDIS']. "</a></td>";
 			echo "<td><a href='gross_net_det.php?sde=SSA&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH&order_type=DIS&order_Sub_type=BB&sdca=" .$data['SDE']. "' target='_blank'>" .$data['BBONLYDIS']. "</a></td>";
 			echo "<td><a href='gross_net_det.php?sde=SSA&fdate=" .$fdate. "&tdate=" .$tdate. "&service=FTTH&order_type=DIS&order_Sub_type=LLBB&sdca=" .$data['SDE']. "' target='_blank'>" .$data['DIS']. "</a></td>";
 			echo "<td>" .$net_result. "</td>";
 			
 
 			echo "</tr>";
-			$llonlyprov= $llonlyprov + $data['LLONLYPROV']; 
 			$bbonlyprov= $bbonlyprov + $data['BBONLYPROV']; 
-			$prov= $prov+ $data['PROV'];
-			$llonlydis= $llonlydis + $data['LLONLYDIS']; 
+			$prov= $prov+ $data['PROV']; 
 			$bbonlydis= $bbonlydis + $data['BBONLYDIS']; 
 			$dis= $dis + $data['DIS'];
 			$net= $net+ $net_result;
@@ -238,10 +232,8 @@ WHERE A.SDE=B.SDE";
 		echo '<tr>';
 		echo "<th colspan='2'> SSA</th>";
 		echo "<th>". $ftth_tar ."</a></th>";
-		echo "<th>". $llonlyprov ."</a></th>";
 		echo "<th>". $bbonlyprov ."</a></th>";
 		echo "<th>". $prov ."</a></th>";
-		echo "<th>". $llonlydis ."</a></th>";
 		echo "<th>". $bbonlydis ."</a></th>";
 		echo "<th>". $dis ."</a></th>";
 		echo "<th>". $net ."</a></th>";
